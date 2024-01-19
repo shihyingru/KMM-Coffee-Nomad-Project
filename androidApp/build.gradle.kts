580556f1f528e31198675fcb91f8f0d261420a6e
yingrushih <shihyingru@gmail.com>
2024-01-19 17:13:04 +0800

    [Modify] Android Target Version to 34 & Gradle Plugin Version to 8.1

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.kaleidoscope.coffeenomad.android"
-    compileSdk = 33
+    compileSdk = 34
     defaultConfig {
         applicationId = "com.kaleidoscope.coffeenomad.android"
         minSdk = 24
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,16 @@ plugins {
     //trick: for the same plugin versions in all sub-modules
     id("com.android.application").version("8.0.1").apply(false)
     id("com.android.library").version("8.0.1").apply(false)
-    id("org.jetbrains.compose").version("1.4.1").apply(false)
+    id("org.jetbrains.compose").version("1.5.11").apply(false)
+//    id("dev.icerock.mobile.multiplatform-resources").version("0.23.0").apply(false)
     kotlin("android").version("1.8.20").apply(false)
     kotlin("multiplatform").version("1.8.20").apply(false)
+}
+
+buildscript {
+    dependencies {
+        classpath("dev.icerock.moko:resources-generator:0.23.0")
+    }
 }
 
 tasks.register("clean", Delete::class) {
